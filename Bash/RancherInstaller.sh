e4793d53d83f00c351ff1dd4383f1d39c9509e57
wonseok <wonseok0403@naver.com>
2018-08-16 09:44:08 +0000

    Rancher Installer updated

--- Bash/RancherInstaller.sh
+++ Bash/RancherInstaller.sh
@@ -1,2 +1,13 @@
-sudo apt-get remove docker docker-engine docker.io -y
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update -y
+sudo apt-get install docker-ce -y
 sudo docker run -d --restart=unless-stopped -p 80:80 -p 443:443 rancher/rancher
